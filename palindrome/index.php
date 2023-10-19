4bd4e8915790acdb17e27b0020001b2e8b2e0782
chouenne <134743005+chouenne@users.noreply.github.com>
2023-10-19 19:22:33 -0400

    edit index.php

--- palindrome/index.php
+++ palindrome/index.php
@@ -26,26 +26,44 @@
 
 //crate a function that checks if a word is a palindrome, using only basics
 
-function palindromeWord($word)
-{
-    // if ($word[0] == $word[length]) & ($word[1] == $word[length-1])
+// function palindromeWord($word)
+// {
+//     // if ($word[0] == $word[length]) & ($word[1] == $word[length-1])
 
-    $length = strlen($word);
-    for ($i = 0; $i < $length / 2; $i++) {
-        if ($word[$i] != $word[($length - 1) - $i]) {
-            return false;
-        }
-    }
-    return true;
+//     $length = strlen($word);
+//     for ($i = 0; $i < $length / 2; $i++) {
+//         if ($word[$i] != $word[($length - 1) - $i]) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+// $word = "mbgvfm";
+// // echo strlen($word);
+// // echo $word[2];
+// // echo $word[3];
+
+// if (palindromeWord($word)) {
+//     echo "$word is a palindrome.";
+// } else {
+//     echo "$word is not a palindrome.";
+// }
+
+
+function isPalindrome($str)
+{
+    // Remove spaces and punctuation and convert to lowercase
+    $cleanedStr = strtolower(preg_replace('/[^a-z]+/', '', $str));
+
+    // Check if the cleaned string is the same forwards and backwards
+    return $cleanedStr === strrev($cleanedStr);
 }
 
-$word = "mbgvfm";
-// echo strlen($word);
-// echo $word[2];
-// echo $word[3];
-
-if (palindromeWord($word)) {
-    echo "$word is a palindrome.";
+// Example usage
+$sentence = "A man, a plan, a canal: Panama";
+if (isPalindrome($sentence)) {
+    echo "The sentence is a palindrome!";
 } else {
-    echo "$word is not a palindrome.";
+    echo "The sentence is not a palindrome.";
 }
