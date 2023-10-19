df961856bc395b0b1f9bc4ac4cb9203643f30298
chouenne <134743005+chouenne@users.noreply.github.com>
2023-10-19 13:52:25 -0400

    index.php

---
+++ palindrome/index.php
@@ -0,0 +1,51 @@
+<?php
+
+// <style>
+//     .even {
+//         background-color: aqua;
+//     }
+
+//     p:nth-child(even) {
+//         background-color: aliceblue;
+//     }
+// </style> 
+
+// $x = 17;
+// $y = 5;
+// echo $x % $y;
+
+// if($x % $y) {
+//     echo "Odd";}
+
+// $sampleArray = array(1, 2, 3, "red", "yellow", "blue");
+
+// for ($i = 0; $i < count($sampleArray); $i++) {
+//     $class = ($i) % 2 ? "odd" : "even";
+//     echo "<p class='$class'>index $i contains: $sampleArray[$i]</p>";
+// }
+
+//crate a function that checks if a word is a palindrome, using only basics
+
+function palindromeWord($word)
+{
+    // if ($word[0] == $word[length]) & ($word[1] == $word[length-1])
+
+    $length = strlen($word);
+    for ($i = 0; $i < $length / 2; $i++) {
+        if ($word[$i] != $word[($length - 1) - $i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+$word = "mbgvfm";
+// echo strlen($word);
+// echo $word[2];
+// echo $word[3];
+
+if (palindromeWord($word)) {
+    echo "$word is a palindrome.";
+} else {
+    echo "$word is not a palindrome.";
+}
