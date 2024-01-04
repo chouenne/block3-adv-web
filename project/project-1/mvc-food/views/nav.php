5dfb53bf0f1969facf3300ec36bd7e2b5fd95067
xuan <2395088@edu.vaniercollege.qc.ca>
2024-01-03 22:31:14 -0500

    update the MVC food folder in project 1

--- project/project-1/mvc-food/views/nav.php
+++ project/project-1/mvc-food/views/nav.php
@@ -2,15 +2,13 @@
   <div class="container">
     <div class="d-flex justify-content-between" style="margin: 20px 0">
       <h1>Food MVC project</h1>
-      <a href="?" class="btn btn-primary  ">Home</a>
+      <a href="?" class="btn btn-primary">Home</a>
     </div>
-
-
 
 
     <div class="row">
       <div class="col-lg-4 col-sm-12 mb-3 mb-sm-0">
-        <div class="card">
+        <div class="card h-100">
           <div class="card-body">
             <h5 class="card-title">Dishes</h5>
             <p class="card-text">Here is the block for adding and displaying all the dishes of the restaurant</p>
@@ -25,7 +23,7 @@
       </div>
 
       <div class="col-lg-4 col-sm-12">
-        <div class="card">
+        <div class="card h-100">
           <div class="card-body">
             <h5 class="card-title">Ingredients</h5>
             <p class="card-text">Here is the block for adding and displaying all the ingredients for preparing the
@@ -39,10 +37,10 @@
       </div>
 
       <div class="col-lg-4 col-sm-12">
-        <div class="card">
+        <div class="card h-100">
           <div class="card-body">
             <h5 class="card-title">Suppliers</h5>
-            <p class="card-text">This is the block for adding and dispaying all the suppliers and their infos that
+            <p class="card-text">This is the block for adding and dispaying all the suppliers that
               supply the ingredients for the restaurant</p>
             <div class="d-flex justify-content-between">
               <a href="?page=suppliers" class="btn btn-warning">Check</a>
